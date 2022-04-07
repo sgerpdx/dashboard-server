aec07f330e455530bf2221401c9e293165d72a83
SamuelHGerber <henrylightfoot@gmail.com>
2022-04-07 16:36:56 -0700

    node-fetch instaled

---
+++ bear.js
@@ -0,0 +1,5 @@
+function logBear() {
+  console.log("Bear");
+}
+
+logBear();
---
+++ mediastackAPI.js
@@ -0,0 +1,10 @@
+import fetch from 'node-fetch';
+
+export const getNews = async () => {
+  const response = await fetch(
+    "http://api.mediastack.com/v1/news?access_key=ebcb8882d755d1ee0fcaf0aea55c0e52"
+  );
+  const data = await response.json();
+
+  console.log("Fetch:", data);
+};
