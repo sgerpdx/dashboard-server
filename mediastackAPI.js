a67168843109a25283ab4d3d63de40d8eb16c5fa
SamuelHGerber <henrylightfoot@gmail.com>
2022-04-07 18:39:00 -0700

    news API get route and munge function

--- mediastackAPI.js
+++ mediastackAPI.js
@@ -1,12 +1,35 @@
 const fetch = require("node-fetch");
 
+// Test API Fetch:
+
 const getNews = async () => {
-  const response = await fetch(
-    "http://api.mediastack.com/v1/news?access_key=ebcb8882d755d1ee0fcaf0aea55c0e52"
-  );
+  const response = await fetch("url");
   const data = await response.json();
 
   console.log("Fetch:", data);
 };
 
-getNews();
+// Munging Function:
+
+const formatNewsResponse = async (resObj) => {
+  // GET from the API endpoint for random news
+  // const response = await fetch("url");
+  // const resObj = await response.json();
+  // const stories = resObj.data;
+  // Map through the response data and munge
+  const formattedNews = resObj.data.map((story) => {
+    return {
+      author: story.author,
+      title: story.title,
+      source: story.source,
+      description: story.description,
+      url: story.url,
+      image: story.image,
+      published: story.published_at,
+    };
+  });
+  console.log("-----FN:", formattedNews);
+  return formattedNews;
+};
+
+module.exports = { getNews, formatNewsResponse };
