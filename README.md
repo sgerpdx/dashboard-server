1b07aa4a999583931e0250de23425be089b50a1e
SamuelHGerber <henrylightfoot@gmail.com>
2022-04-12 15:07:38 -0700

    README update

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-## Server for Personal Web Dashboard
+## Dashboard Server
 
-Basic functionality is CRUD routes for a database storing user-generated bookmark and notepad data.
+This repo acts as a server for [Personal Web Dashboard](https://github.com/sgerpdx/personal-web-dashboard) which is intended to be a use-in-any-browser app for storing personal web bookmarks, as well as accessing local time and eventually, weather and news.
+
+Currently this code provides for three endpoints:
+
+- /bookmarks -- Fully accessible via the client-side code for CRUD routes.
+  - /bookmarks/:id -- Usable with GET, PUT and DELETE.
+- /notes -- Meant for recording miscellaneous info for personal use. Not yet implemented in client.
+- /news -- Runs a simple fetch to external [Mediastack](https://mediastack.com/) API for news.
+
+As of yet user account/auth functionality has not been added to this code.
--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,98 @@
 require("dotenv").config();
+const app = require("../lib/app.js");
+const pool = require("../lib/utils/pool");
+const supertest = require("supertest");
+//const { bookmarksData } = require("../data/bookmarks");
 
 describe("postgresSQL database CRUD routes", () => {
   //beforeAll
   //afterAll
+
+  beforeEach(() => {
+    pool.connect();
+  });
+
+  afterEach(() => {
+    app.close();
+  });
 
   // Tests
   it("verifies that Jest is working", () => {
     expect(1).toBe(1);
   });
 
-//   it("returns all bookmarks", async () => {
-//     //code
-//   });
+  it("returns all bookmarks", async () => {
+    //
+    const expectation = [
+      {
+        id: 1,
+        bookmarkTitle: "place kitten",
+        bookmarkURL: "https://placekitten.com/200/300",
+        dateCreated: "2022-04-04T07:00:00.000Z",
+      },
+      {
+        id: 4,
+        bookmarkTitle: "neocities",
+        bookmarkURL: "https://neocities.org/",
+        dateCreated: "2022-04-05T07:00:00.000Z",
+      },
+      {
+        id: 6,
+        bookmarkTitle: "The AV Yacht Club",
+        bookmarkURL: "https://www.avclub.com/",
+        dateCreated: "2022-04-06T07:00:00.000Z",
+      },
+      {
+        id: 13,
+        bookmarkTitle: "Toyota",
+        bookmarkURL: "http://www.toyota.com",
+        dateCreated: "2022-04-09T07:00:00.000Z",
+      },
+      {
+        id: 14,
+        bookmarkTitle: "Subaru",
+        bookmarkURL: "http://www.subaru.com",
+        dateCreated: "2022-04-09T07:00:00.000Z",
+      },
+      {
+        id: 19,
+        bookmarkTitle: "Suzuki",
+        bookmarkURL: "http://www.suzuki.com",
+        dateCreated: "2022-04-09T07:00:00.000Z",
+      },
+      {
+        id: 20,
+        bookmarkTitle: "Lexus",
+        bookmarkURL: "http://www.lexus.com",
+        dateCreated: "2022-04-09T07:00:00.000Z",
+      },
+      {
+        id: 22,
+        bookmarkTitle: "Ford",
+        bookmarkURL: "http://www.ford.com",
+        dateCreated: "2022-04-09T07:00:00.000Z",
+      },
+      {
+        id: 23,
+        bookmarkTitle: "Chrysler",
+        bookmarkURL: "http://www.chrysler.com",
+        dateCreated: "2022-04-09T07:00:00.000Z",
+      },
+      {
+        id: 24,
+        bookmarkTitle: "Star Wars",
+        bookmarkURL: "http://www.starwars.com",
+        dateCreated: "2022-04-09T07:00:00.000Z",
+      },
+    ];
+
+    //
+    const data = await supertest(app)
+      .get("/api/v1/bookmarks")
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    //
+    expect(data.body).toEqual(expectation);
+  });
 });
