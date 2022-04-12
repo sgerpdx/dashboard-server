2055f04188e81a1ccaf963e38c0dda645b69cb4c
SamuelHGerber <henrylightfoot@gmail.com>
2022-04-12 11:47:50 -0700

    test file created and basic Jest test mechanism working per 1=1 test

---
+++ __tests__/app.test.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+describe("postgresSQL database CRUD routes", () => {
+  //beforeAll
+  //afterAll
+
+  // Tests
+  it("verifies that Jest is working", () => {
+    expect(1).toBe(1);
+  });
+
+//   it("returns all bookmarks", async () => {
+//     //code
+//   });
+});
